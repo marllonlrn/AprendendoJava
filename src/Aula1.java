c580543fb99d7ff6c0e7b427f8a4f47a551186c3
Marllon Lorran <marllonlrn300@gmail.com>
2026-09-02 16:05:34 -0300

    feat: adiciona exercício de estruturas condicionais

--- src/Aula1.java
+++ src/Aula1.java
@@ -1,4 +1,4 @@
-public class Main {
+public class Aula1 {
     public static void main(String[] args) {
         String name = "Marllon Lorran";
         String cidade = "João Pessoa";
---
+++ src/Aula2.java
@@ -0,0 +1,13 @@
+public class Aula2 {
+    public static void main(String[] args) {
+      float numero1 = 10.5f;
+
+      if(numero1 > 0){
+        System.out.println("O número é positivo.");
+      } else if(numero1 < 0){
+        System.out.println("O número é negativo.");
+      } else {
+        System.out.println("O número é zero.");
+      }
+    }
+}
