89074b72d30d66e4838462152a8507422bec8eb7
Marllon Lorran <marllonlrn300@gmail.com>
2026-09-02 22:19:47 -0300

    feat: adiciona introdução básica a POO

---
+++ src/Aula3.java
@@ -0,0 +1,33 @@
+public class Aula3 {
+  public static void main(String[] args) {
+    // Um objeto e uma instancia criada a partir de uma classe.
+    Pessoa aluno = new Pessoa("Marllon", 20);
+
+    aluno.apresentar();
+    aluno.fazerAniversario();
+    aluno.apresentar();
+  }
+}
+
+// A classe funciona como um molde para criar objetos.
+class Pessoa {
+  // Atributos guardam as caracteristicas do objeto.
+  private String nome;
+  private int idade;
+
+  // O construtor define os valores iniciais do objeto.
+  public Pessoa(String nome, int idade) {
+    this.nome = nome;
+    this.idade = idade;
+  }
+
+  // Metodos representam comportamentos do objeto.
+  public void apresentar() {
+    System.out.println("Ola! Meu nome e " + nome + " e tenho " + idade + " anos.");
+  }
+
+  public void fazerAniversario() {
+    idade++;
+    System.out.println(nome + " fez aniversario!");
+  }
+}
